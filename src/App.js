7cf13d8b61524abeb10716da64f0fcbb805d06ff
SagarTalagatti <sagartalagatti@gmail.com>
2023-05-23 10:19:37 +0530

    Completed the first practice project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from "react";
 
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (username, age) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, { name: username, age: age }];
+    });
+  };
+
   return (
     <div>
-
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
